f76ba8660f5914b543f9518ccb266baaab044b36
avasmith98 <162925381+avasmith98@users.noreply.github.com>
2024-11-19 11:07:27 -0600

    Added procfile and changed flask listening port

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn predict_cluster:app
+
--- predict_cluster.py
+++ predict_cluster.py
@@ -75,5 +75,6 @@ def predict():
     }
     return jsonify(output)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
